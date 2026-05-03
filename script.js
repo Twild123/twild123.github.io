e27e6bcfba59e3af8215fc7a9906c0e9156c65a7
Twild <jeffreygodrell@gmail.com>
2026-05-03 03:06:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,63 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const movie_name = document.querySelector("#movie-name");
-    const movie_year = document.querySelector("#movie-year");
-    const movie_page = document.querySelector("#movie-page");
-    const search_button = document.querySelector("#search-button");
-    const billboards = document.getElementById("billboards");
+    const black_screen = document.querySelector("#black-screen");
+    const bg_video = document.querySelector("#bg-video");
 
-    const api_key = "5ef0849d";
-
-    function construirEndpoint() {
-        let s = movie_name.value;
-        let y = movie_year.value;
-        let p = movie_page.value;
-
-        return `https://www.omdbapi.com/?apikey=${api_key}&s=${s}&y=${y}&page=${p}`;
-    }
-
-    async function obtenerPeliculas() {
-        try {
-            let url = construirEndpoint();
-            let response = await fetch(url);
-
-            if (!response.ok) {
-                throw new Error(`HTTP error: ${response.status}`);
-            }
-
-            return await response.json();
-
-        } catch (error) {
-            document.querySelector("#total-results").textContent = `Resultados totales: 0`;
-            billboards.innerHTML = '<p style="color:white;font-size:140%">Sin resultados</p>';
-            console.error(error);
-        }
-    }
-
-    function crearPortadas(data) {
-        if (data.Response === "False") {
-            document.querySelector("#total-results").textContent = `Resultados totales: 0`;
-            billboards.innerHTML = '<p style="color:white;font-size:140%">Sin resultados</p>';
-            return;
-        }
-
-        const movies = data.Search;
-
-        document.querySelector("#total-results").textContent = `Resultados totales: ${data.totalResults}`;
-
-        billboards.innerHTML = movies.map(m => `
-            <div class="billboard bg-purple br8 of-hidden">
-                <img src="${m.Poster}" class="b-poster">
-                <div class="b-meta bg-gray flex flex-col align-items-flex-start">
-                    <div class="fz16 font-arial">Titulo: ${m.Title}</div>
-                    <div class="fz16 font-arial">Tipo: ${m.Type}</div>
-                    <div class="fz16 font-arial">Año: ${m.Year}</div>
-                </div>
-            </div>
-        `).join('');
-    }
-
-    search_button.addEventListener("click", async () => {
-        let data = await obtenerPeliculas();
-        crearPortadas(data);
-    });
-});
+    black_screen.addEventListener("click", () => { black_screen.style.display = "none"; bg_video.play() })
+})
